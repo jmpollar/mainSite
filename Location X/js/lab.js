3683f294a6f9a481ad2555f83431f40a7fb2e653
jmpollar <103337804+jmpollar@users.noreply.github.com>
2023-05-24 22:34:25 -0700

    got the rects to draw but bg image keeps disappear

--- Location X/js/lab.js
+++ Location X/js/lab.js
@@ -1,7 +1,6 @@
 // index.js - purpose and description here
 // Author: Your Name
 // Date:
-
 // Constants
 //declare the areas as class instances\
 //taking parameters of max/min x/y as bounds
@@ -22,32 +21,83 @@ function myFunction(param1, param2) {
 }
 
 function main() {
+  var xOffset = 210;
+  var yOffset = 40;
+  const area1 = new Area(70 + xOffset, 265 + yOffset, 415 + xOffset, 450 + yOffset);
+  const area2 = new Area(40 + xOffset, 770 + yOffset, 390 + xOffset, 940 + yOffset);
+  const area3 = new Area(550 + xOffset, 690 + yOffset, 670 + xOffset, 900 + yOffset);
+  const area4 = new Area(1280 + xOffset, 340 + yOffset, 1465 + xOffset, 630 + yOffset);
+  const area5 = new Area(1125 + pageXOffset, 750 + yOffset, 1480 + xOffset, 930 + yOffset);
+  function drawBoxes() {
+    //draw the rectangles to ensure areas are consistent WILL REMOVE LATER
+    $("#img-canvas").drawRect({
+      name: "box1",
+      layer: true,
+      strokeStyle: 'red',
+      strokeWidth: 4,
+      x: area1.minX, y: area1.minY,
+      width: (area1.maxX - area1.minX),
+      height: (area1.maxY - area1.minY),
+      fromCenter: false
+    });
+    //draw the rectangles to ensure areas are consistent WILL REMOVE LATER
+    $("#img-canvas").drawRect({
+      name: "box2",
+      layer: true,
+      strokeStyle: 'red',
+      strokeWidth: 4,
+      x: area2.minX, y: area2.minY,
+      width: (area2.maxX - area2.minX),
+      height: (area2.maxY - area2.minY),
+      fromCenter: false,
+    });
+    //draw the rectangles to ensure areas are consistent WILL REMOVE LATER
+    $("#img-canvas").drawRect({
+      name: "box3",
+      layer: true,
+      strokeStyle: 'red',
+      strokeWidth: 4,
+      x: area3.minX, y: area3.minY,
+      width: (area3.maxX - area3.minX),
+      height: (area3.maxY - area3.minY),
+      fromCenter: false
+    });
+    //draw the rectangles to ensure areas are consistent WILL REMOVE LATER
+    $("#img-canvas").drawRect({
+      name: "box4",
+      layer: true,
+      strokeStyle: 'red',
+      strokeWidth: 4,
+      x: area4.minX, y: area4.minY,
+      width: (area4.maxX - area4.minX),
+      height: (area4.maxY - area4.minY),
+      fromCenter: false
+    });
+    //draw the rectangles to ensure areas are consistent WILL REMOVE LATER
+    $("#img-canvas").drawRect({
+      name: "box5",
+      layer: true,
+      strokeStyle: 'red',
+      strokeWidth: 4,
+      x: area5.minX, y: area5.minY,
+      width: (area5.maxX - area5.minX),
+      height: (area5.maxY - area5.minY),
+      fromCenter: false
+    });
+  }
   //draw the image on the canvas using jCanvas at center of page 
   $("#img-canvas").drawImage({
     source: 'img/photo1.png',
     x: 210,
     y: 40,
     scale: 1,
-    fromCenter: false
+    fromCenter: false,
+    load: drawBoxes
   });
-  const area1 = new Area(70, 265, 415, 450);
-  const area2 = new Area(40, 770, 390, 940);
-  const area3 = new Area(550, 690, 670, 900);
-  const area4 = new Area(1280, 340, 1465, 630);
-  const area5 = new Area(1125, 750, 1480, 930);
-
-   //draw the rectangles to ensure areas are consistent WILL REMOVE LATER
-   $("#img-canvas").drawRect({
-    strokeStyle: 'red',
-    strokeWidth: 4,
-    x: area1.minX, y: area1.minY,
-    width: (area1.maxX-area1.minX),
-    height: (area1.maxY-area1.minY)
-  });
-  // the code that makes everything happen
-  let canv = $("#img-canvas");
 
 }
+
+
 
 // let's get this party started
 main();
