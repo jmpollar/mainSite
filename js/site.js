98748d7924eed2257eb639f057561cfa8079f27e
jmpollar <103337804+jmpollar@users.noreply.github.com>
2023-06-06 12:59:56 -0700

    js organzign

--- js/site.js
+++ js/site.js
@@ -3,6 +3,55 @@
 // Date:
 //global var imageEl
 var imageEl;
+var gameStarted = false;
+var startTime = Date.now();
+var timeElapsed = Date.now();
+var rect1, rect2, rect3, rect4, rect5;
+var quaddie1, quaddie2, quaddie3, quaddie4;
+var imgCanvas = $("#img-canvas");
+var currentQuadrant;
+var photo1Width = 1920;
+var photo1Height = 1080;
+var gameOver = false;
+var timeElapsed = 0.0;
+var slugWidth = 50;
+var slugHeight = 47;
+var numberOfAreas = 5;
+var xOffset = 0;
+var yOffset = 0;
+
+//hide img canvas until game start
+imgCanvas.hide();
+
+// Imported from Location X JS 
+//create a clock to track time
+class Area {
+  constructor(minX, minY, maxX, maxY) {
+    this.minX = minX;
+    this.minY = minY;
+    this.maxX = maxX;
+    this.maxY = maxY;
+  }
+}
+
+const area1 = new Area(70 + xOffset, 265 + yOffset, 415 + xOffset, 450 + yOffset);
+const area2 = new Area(40 + xOffset, 770 + yOffset, 390 + xOffset, 940 + yOffset);
+const area3 = new Area(550 + xOffset, 690 + yOffset, 670 + xOffset, 900 + yOffset);
+const area4 = new Area(1280 + xOffset, 340 + yOffset, 1465 + xOffset, 630 + yOffset);
+const area5 = new Area(1125 + xOffset, 750 + yOffset, 1480 + xOffset, 930 + yOffset);
+
+//declare quadrants
+const quad1 = new Area(xOffset, yOffset, xOffset + (photo1Width / 2), yOffset + (photo1Height / 2));
+const quad2 = new Area(xOffset + (photo1Width / 2), yOffset, xOffset + (photo1Width), yOffset + (photo1Height / 2));
+const quad3 = new Area(xOffset, yOffset + (photo1Height / 2), xOffset + (photo1Width / 2), yOffset + (photo1Height));
+const quad4 = new Area(xOffset + (photo1Width / 2), yOffset + (photo1Height / 2), xOffset + (photo1Width), yOffset + (photo1Height));
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//determine teh random coordinates
+var coords = chooseCoordinates();
+
 
 // When a tab is clicked
 $('.tab').click(function () {
@@ -21,7 +70,56 @@ $('.tab').click(function () {
   // specific location tabs
   if (tabId === 'tab3') {
     $(".nav-bar").hide();
-    $('body').css({'background-image' : "url('img/photo1.png')", 'background-repeat' : 'no-repeat'});
+    $('body').css('background-image', "url('img/photo1.png')");
+    imgCanvas.show();
+    //set gameStarted to true
+    gameStarted = true;
+
+    console.log("GAME STARTED: " + gameStarted);
+  }
+
+
+});
+
+$("#hideSimpleBtn").click(function () {
+  $("#simpleDiv").hide();
+});
+
+$("#showSimpleBtn").click(function () {
+  $("#simpleDiv").show();
+});
+
+//KIND OF works, but changing the window size changes click position which is an issue, if only we could make the image an el at a specific spot.
+/*$("#img-canvas").click(function (clickPos) {
+  console.log(clickPos.clientX + " " + clickPos.clientY);
+  //when the slug is clicked, event listener
+  if (gameStarted === true && (clickPos.clientX <= coords[2] + 100 && clickPos.clientX >= coords[2] - 100) && (clickPos.clientY <= coords[3] + 100 && clickPos.clientY >= coords[3] - 100)) {
+    console.log("HIT");
+    var ctx = imgCanvas.getContext("2d");
+    //hide the slug
+    ctx.clearRect(0, 0, $("#img-canvas").width, $("#img-canvas").height);
+  }
+
+});*/
+
+// When a tab is clicked
+$('.tab').click(function () {
+  // Get the value of the data-tab attribute of the clicked tab
+  var tabId = $(this).data('tab');
+
+  // Remove the 'active' class from all tabs
+  $('.tab').removeClass('active');
+  // Add the 'active' class to the clicked tab
+  $(this).toggleClass('active');
+  // Hide all tab panes
+  $('.tab-pane').removeClass('active');
+  // Show the tab pane corresponding to the clicked tab
+  $('[data-tab-content="' + tabId + '"]').toggleClass('active');
+
+  // specific location tabs
+  if (tabId === 'tab3') {
+    $(".nav-bar").hide();
+    $('body').css({ 'background-image': "url('img/photo1.png')", 'background-repeat': 'no-repeat' });
     /*   $('body').css({'background-image' : "url('img/photo1.png')", 'background-position' : 'center', 'background-size' : 'cover', 'position' : 'relative', 'width' : '100%', 'height' : '100%'});*/
     //////////////// turn this into a function ////////////////////////
     // Define image URL and coords
@@ -32,53 +130,16 @@ $('.tab').click(function () {
     imageEl = $('<img>');
     imageEl.attr('src', imageUrl);
     imageEl.attr('id', "slug1");
-    drawSluggy();
-
     ///////////////////////////////////////
-
+    //call drawSluggy now for testing but this will be called with an html listener of sorts later
+    //get random coordinates from the function
+    drawSluggy();
+    //drawBoxes();
+    //after 30 seconds, highlight the quadrant the slug is in
+    const showQuadTimeout = setTimeout(drawQuadrant, 9000, currentQuadrant);
   }
 })
 
-// Imported from Location X JS 
-//create a clock to track time
-class Area {
-  constructor(minX, minY, maxX, maxY) {
-    this.minX = minX;
-    this.minY = minY;
-    this.maxX = maxX;
-    this.maxY = maxY;
-  }
-}
-var startTime = Date.now();
-var timeElapsed = Date.now();
-var rect1, rect2, rect3, rect4, rect5;
-var quaddie1, quaddie2, quaddie3, quaddie4;
-var imgCanvas = $("#img-canvas");
-var coords;
-var currentQuadrant;
-var photo1Width = 1500;
-var photo1Height = 1000;
-var gameOver = false;
-var timeElapsed = 0.0;
-var slugWidth = 1170.0;
-var slugHeight = 1097.0;
-var numberOfAreas = 5;
-var xOffset = 210;
-var yOffset = 300;
-const area1 = new Area(70 + xOffset, 265 + yOffset, 415 + xOffset, 450 + yOffset);
-const area2 = new Area(40 + xOffset, 770 + yOffset, 390 + xOffset, 940 + yOffset);
-const area3 = new Area(550 + xOffset, 690 + yOffset, 670 + xOffset, 900 + yOffset);
-const area4 = new Area(1280 + xOffset, 340 + yOffset, 1465 + xOffset, 630 + yOffset);
-const area5 = new Area(1125 + xOffset, 750 + yOffset, 1480 + xOffset, 930 + yOffset);
-
-//declare quadrants
-const quad1 = new Area(xOffset, yOffset, xOffset + (photo1Width / 2), yOffset + (photo1Height / 2));
-const quad2 = new Area(xOffset + (photo1Width / 2), yOffset, xOffset + (photo1Width), yOffset + (photo1Height / 2));
-const quad3 = new Area(xOffset, yOffset + (photo1Height / 2), xOffset + (photo1Width / 2), yOffset + (photo1Height));
-const quad4 = new Area(xOffset + (photo1Width / 2), yOffset + (photo1Height / 2), xOffset + (photo1Width), yOffset + (photo1Height));
-function getRandomInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 //define function that randomly selects an area and selects coordinates based off that
 function chooseCoordinates() {
@@ -117,8 +178,6 @@ function chooseCoordinates() {
 }
 
 function drawSluggy() {
-  //get random coordinates from the function
-  coords = chooseCoordinates();
   console.log(coords); //functional
   console.log(coords[2] + coords[3]);
 
@@ -139,8 +198,8 @@ function drawSluggy() {
     // (it can be like the start button where that is what makes it go to the next tab)
   });
 }
-/*
-function drawBoxes() {
+
+/*function drawBoxes() {
   //draw the rectangles to ensure areas are consistent WILL REMOVE LATER
   var ctx = imgCanvas.getContext("2d");
   ctx.beginPath();
@@ -162,11 +221,7 @@ function drawBoxes() {
 
   rect5 = ctx.rect(area5.minX, area5.minY, (area5.maxX - area5.minX), (area5.maxY - area5.minY));
   ctx.stroke();
-}
-
-
-//call drawSluggy now for testing but this will be called with an html listener of sorts later
-drawSluggy();
+}*/
 
 //find out what quadrant the slug is in
 if (coords[2] >= xOffset && coords[3] >= yOffset && coords[2] <= xOffset + (photo1Width / 2) && coords[3] <= yOffset + (photo1Height / 2)) {
@@ -215,74 +270,4 @@ function drawQuadrant(quad) {
   }
 }
 
-// When a tab is clicked
-$('.tab').click(function () {
-  // Get the value of the data-tab attribute of the clicked tab
-  var tabId = $(this).data('tab');
 
-  // Remove the 'active' class from all tabs
-  $('.tab').removeClass('active');
-  // Add the 'active' class to the clicked tab
-  $(this).toggleClass('active');
-  // Hide all tab panes
-  $('.tab-pane').removeClass('active');
-  // Show the tab pane corresponding to the clicked tab
-  $('[data-tab-content="' + tabId + '"]').toggleClass('active');
-
-  // specific location tabs
-  if (tabId === 'tab3') {
-    $(".nav-bar").hide();
-    $('body').css('background-image', "url('img/photo1.png')");
-  }
-
-  //set gameStarted to true
-  gameStarted = true;
-
-  console.log("GAME STARTED: " + gameStarted);
-});
-
-$("#hideSimpleBtn").click(function () {
-  $("#simpleDiv").hide();
-});
-
-$("#showSimpleBtn").click(function () {
-  $("#simpleDiv").show();
-});
-var gameStarted = false;
-var startTime;
-//when start button clicked
-$("#start").click(function () {
-  //after 30 seconds, highlight the quadrant the slug is in
-  const showQuadTimeout = setTimeout(drawQuadrant, 120000, currentQuadrant);
-
-});
-
-//KIND OF works, but changing the window size changes click position which is an issue, if only we could make the image an el at a specific spot.
-$("#img-canvas").click(function (clickPos) {
-  console.log(clickPos.clientX + " " + clickPos.clientY);
-  //when the slug is clicked, event listener
-  if (gameStarted === true && (clickPos.clientX <= coords[2] + 100 && clickPos.clientX >= coords[2] - 100) && (clickPos.clientY <= coords[3] + 100 && clickPos.clientY >= coords[3] - 100)) {
-    console.log("HIT");
-    var ctx = imgCanvas.getContext("2d");
-    //hide the slug
-    ctx.clearRect(0, 0, $("#img-canvas").width, $("#img-canvas").height);
-  }
-
-})
-
-
-$("#hideSimpleBtn").click(function () {
-  $("#simpleDiv").hide();
-});
-
-$("#showSimpleBtn").click(function () {
-  $("#simpleDiv").show();
-});
-var gameStarted = false;
-var startTime;
-//when start button clicked
-$("#start").click(function () {
-  //after 120 seconds, highlight the quadrant the slug is in
-  const showQuadTimeout = setTimeout(drawQuadrant, 120000, currentQuadrant);
-
-});*/
