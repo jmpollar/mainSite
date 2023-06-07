522901a4144bcc3fbe6a194bc3710cdac5703124
jmpollar <103337804+jmpollar@users.noreply.github.com>
2023-06-06 22:21:46 -0700

    timer paused & slug hidden on click

--- js/site.js
+++ js/site.js
@@ -159,7 +159,12 @@ function drawSluggy() {
   console.log("placed");
   // Add an event listener for click
   imageEl.on('click', function () {
+    //print slug fact
     $("#location1").append("<p style= font-size:12px;>“In 1981, UCSC joined Division III of the NCAA. Since the application required an official team name, UCSC’s then-chancellor polled the student players, and out of this small group emerged a consensus for a new moniker—the sea lions. But the new name did not find favor with the majority of students, who continued to root for the Slugs. After five years of dealing with the two-mascot problem, an overwhelming pro-Slug straw vote by students in 1986 convinced the chancellor to make the lowly but beloved Banana Slug UCSC’s official mascot.”</p>");
+    //hide the slug
+    imageEl.hide();
+    //pause the timer
+    timerNew.pause();
     // maybe have a button to move to the next location 
     // (it can be like the start button where that is what makes it go to the next tab)
   });
